5ac53564707d327c7ad076b6f9009a6bc623c6b4
kitamuraDev <masato1999824@icloud.com>
2022-05-08 18:40:42 +0900

    :lipstick: スタイル調整 done

--- src/App.tsx
+++ src/App.tsx
@@ -22,24 +22,19 @@ const App: VFC = () => {
       .catch((err) => console.error(err));
   }, []);
 
-  console.log(users);
-
   return (
     <div className='App'>
       <div className='main'>
         <h2>検索アプリ</h2>
         <input type='text' ref={input} />
         <div className='content'>
-          <div className='box'>
-            <h3>ユーザー名</h3>
-            <hr />
-            <p>メールアドレス</p>
-          </div>
-          <div className='box'>
-            <h3>ユーザー名</h3>
-            <hr />
-            <p>メールアドレス</p>
-          </div>
+          {users.map((user) => (
+            <div className='box' key={user.id}>
+              <h3>{user.name}</h3>
+              <hr />
+              <p>{user.email}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
