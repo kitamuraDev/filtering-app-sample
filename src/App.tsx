24d61de1b41c7653e6da47921a06cbed6d6bb490
kitamuraDev <masato1999824@icloud.com>
2022-05-08 18:15:19 +0900

    :sparkles: レスポンスの型定義とfetch処理を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { useEffect, useRef, VFC } from 'react';
+/* eslint-disable no-console */
+import { useEffect, useRef, useState, VFC } from 'react';
+
+import axios from 'axios';
+
+import { User } from 'types/User';
 
 const App: VFC = () => {
   const input = useRef<HTMLInputElement>(null);
+  const [users, setUsers] = useState<User[]>([]);
 
+  // focus
   useEffect(() => {
     input.current?.focus();
   }, []);
+
+  // axios
+  useEffect(() => {
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  console.log(users);
 
   return (
     <div className='App'>
