9ac9376ccdfa3fd223c242ccbf1855ff9a6537eb
kitamuraDev <masato1999824@icloud.com>
2022-05-08 17:47:22 +0900

    :sparkles: アプリの雛形を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import { VFC } from 'react';
+import { useEffect, useRef, VFC } from 'react';
 
-const App: VFC = () => (
-  <div className='App'>
-    <p>Hello React</p>
-  </div>
-);
+const App: VFC = () => {
+  const input = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    input.current?.focus();
+  }, []);
+
+  return (
+    <div className='App'>
+      <div className='main'>
+        <h2>検索アプリ</h2>
+        <input type='text' ref={input} />
+        <div className='content'>
+          <div className='box'>
+            <h3>ユーザー名</h3>
+            <hr />
+            <p>メールアドレス</p>
+          </div>
+          <div className='box'>
+            <h3>ユーザー名</h3>
+            <hr />
+            <p>メールアドレス</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
